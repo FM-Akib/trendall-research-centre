746990405c28c243fda45f003539f74c3c3e45fc
FM-Akib <muntasirakib08@gmail.com>
2024-04-21 19:30:41 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './pages/Layout/Layout';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import News from './pages/News/News';
+import Search from './pages/Search/Search';
+import BasicSearch from './pages/BasicSearch/BasicSearch';
+import AdvancedSearch from './pages/AdvancedSearch/AdvancedSearch';
+import IndividualCard from './pages/AdvancedSearch/IndividualCard';
+import ANews from './pages/News/ANews';
+import SearchLayout from './pages/Layout/SearchLayout';
+import SearchTechnique from './pages/SearchTechnique/SearchTechnique';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path:"/",
+        element: <Home></Home>
+      },
+      {
+        path:"about",
+        element: <About></About>
+      },
+      {
+        path:"news",
+        element: <News></News>
+      },
+      {
+        path:"news/:id",
+        element: <ANews></ANews>,
+        loader: ()=> fetch('news.json')
+      },
+      {
+        path:"search",
+        element: <Search></Search>
+      },
+      {
+        path:"basicsearch",
+        element: <BasicSearch></BasicSearch>
+      },
+      {
+        path:"advancedsearch",
+        element: <AdvancedSearch></AdvancedSearch>
+      },
+      {
+        path:"advancedsearch/:id",
+        element: <IndividualCard></IndividualCard>,
+        loader: ()=> fetch('dummyFabric.json')
+      }
+    ]
+  },
+  {
+    path:'searchtec',
+    element: <SearchLayout></SearchLayout>,
+    children: [
+      {
+        path: 'all',
+        element: <SearchTechnique></SearchTechnique>
+      }
+    ]
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/Layout/SearchLayout.jsx
@@ -0,0 +1,192 @@
+import { useState } from "react";
+import Navbar from "../../components/Navbar";
+import { Link, Outlet } from "react-router-dom";
+
+const SearchLayout = () => {
+    const [openAnswers, setOpenAnswers] = useState({});
+
+    // Function to toggle the visibility of the answer
+    const toggleAnswer = (targetId) => {
+        setOpenAnswers(prevState => ({
+            ...prevState,
+            [targetId]: !prevState[targetId] // Toggle the visibility of the target answer
+        }));
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className="grid grid-cols-10 w-[80%] mx-auto">
+                <div className="col-span-3 bg-white h-screen">
+                    <div className="bg-white pt-14">
+                        <section className="max-w-5xl mx-auto">
+                            <div className="w-full px-7 md:px-10 xl:px-2 bg-white">
+                                <div className="mx-auto w-full max-w-5xl border border-slate-400/20 rounded-lg bg-white ">
+                                    <div className="border-b border-[#0A071B]/10">
+                                        <button
+                                            className="question-btn flex w-full items-start gap-x-5 justify-between rounded-lg text-left text-lg font-bold text-slate-800 focus:outline-none p-5"
+                                            onClick={() => toggleAnswer('answer-1')} >
+                                            <span>Show only</span>
+                                            <svg className={`mt-1.5 md:mt-0 flex-shrink-0 transform h-5 w-5 text-[#5B5675] ${
+                                                openAnswers['answer-1'] ? 'rotate-180' : '' // Rotate if this answer is open
+                                            }`}
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+                                            </svg>
+                                        </button>
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-1'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-1"
+                                        >
+                                            <Link to="" className="hover:text-red-500">Peer-reviewed (1222)</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        
+                        <section className="max-w-5xl mx-auto ">
+                            <div className="w-full px-7 md:px-10 xl:px-2 pt-4 bg-white">
+                                <div className="mx-auto w-full max-w-5xl border border-slate-400/20 rounded-lg bg-white ">
+                                    <div className="border-b border-[#0A071B]/10">
+                                        <button
+                                            className="question-btn flex w-full items-start gap-x-5 justify-between rounded-lg text-left text-lg font-bold text-slate-800 focus:outline-none p-5"
+                                            onClick={() => toggleAnswer('answer-2')} >
+                                            <span>Resource Types</span>
+                                            <svg className={`mt-1.5 md:mt-0 flex-shrink-0 transform h-5 w-5 text-[#5B5675] ${
+                                                openAnswers['answer-2'] ? 'rotate-180' : '' // Rotate if this answer is open
+                                            }`}
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+                                            </svg>
+                                        </button>
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-2'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-2"
+                                        >
+                                            <Link to="" className="hover:text-red-500">Articles (1222)</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/* Third Section */}
+                        <section className="max-w-5xl mx-auto ">
+                            <div className="w-full px-7 md:px-10 xl:px-2 pt-4 bg-white">
+                                <div className="mx-auto w-full max-w-5xl border border-slate-400/20 rounded-lg bg-white ">
+                                    <div className="border-b border-[#0A071B]/10">
+                                        <button
+                                            className="question-btn flex w-full items-start gap-x-5 justify-between rounded-lg text-left text-lg font-bold text-slate-800 focus:outline-none p-5"
+                                            onClick={() => toggleAnswer('answer-3')} >
+                                            <span>Dates</span>
+                                            <svg className={`mt-1.5 md:mt-0 flex-shrink-0 transform h-5 w-5 text-[#5B5675] ${
+                                                openAnswers['answer-3'] ? 'rotate-180' : '' // Rotate if this answer is open
+                                            }`}
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+                                            </svg>
+                                        </button>
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-3'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-3"
+                                        >
+                                            <Link to="" className="hover:text-red-500">5 September 2015</Link>
+                                        </div>
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-3'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-3"
+                                        >
+                                            <Link to="" className="hover:text-red-500">7 September 2017</Link>
+                                        </div>
+                                        {/* Add more answers as needed */}
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/* Fourth Section */}
+                        <section className="max-w-5xl mx-auto ">
+                            <div className="w-full px-7 md:px-10 xl:px-2 pt-4 bg-white">
+                                <div className="mx-auto w-full max-w-5xl border border-slate-400/20 rounded-lg bg-white ">
+                                    <div className="border-b border-[#0A071B]/10">
+                                        <button
+                                            className="question-btn flex w-full items-start gap-x-5 justify-between rounded-lg text-left text-lg font-bold text-slate-800 focus:outline-none p-5"
+                                            onClick={() => toggleAnswer('answer-4')} >
+                                            <span>Location</span>
+                                            <svg className={`mt-1.5 md:mt-0 flex-shrink-0 transform h-5 w-5 text-[#5B5675] ${
+                                                openAnswers['answer-4'] ? 'rotate-180' : '' // Rotate if this answer is open
+                                            }`}
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+                                            </svg>
+                                        </button>
+                                        {/* Add more answers as needed */}
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-3'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-3"
+                                        >
+                                            <Link to="" className="hover:text-red-500">Australia</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/* Fifth Section */}
+                        <section className="max-w-5xl mx-auto ">
+                            <div className="w-full px-7 md:px-10 xl:px-2 pt-4 bg-white">
+                                <div className="mx-auto w-full max-w-5xl border border-slate-400/20 rounded-lg bg-white ">
+                                    <div className="border-b border-[#0A071B]/10">
+                                        <button
+                                            className="question-btn flex w-full items-start gap-x-5 justify-between rounded-lg text-left text-lg font-bold text-slate-800 focus:outline-none p-5"
+                                            onClick={() => toggleAnswer('answer-5')} >
+                                            <span>Subjects</span>
+                                            <svg className={`mt-1.5 md:mt-0 flex-shrink-0 transform h-5 w-5 text-[#5B5675] ${
+                                                openAnswers['answer-5'] ? 'rotate-180' : '' // Rotate if this answer is open
+                                            }`}
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+                                            </svg>
+                                        </button>
+                                        {/* Add more answers as needed */}
+                                        <div
+                                            className={`answer pt-2 pb-5 px-5 text-sm lg:text-base text-[#343E3A] font-medium ${
+                                                openAnswers['answer-3'] ? '' : 'hidden' // Show if this answer is open
+                                                }`}
+                                            id="answer-3"
+                                        >
+                                            <Link to="" className="hover:text-red-500">Science</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/* Add more sections as needed */}
+                    </div>
+                </div>
+                <div className="col-span-7 pt-14 pl-6">
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SearchLayout;
